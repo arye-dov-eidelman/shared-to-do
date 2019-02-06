82252bb23a5243fbf3950c6e002fa6847e8470f3
Arye Dov Eidelman <aryedov.e@gmail.com>
2019-02-05 20:34:56 -0500

    add bcrypt and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, :password, presence: true
+
+  validates :name, length: { in: 1..50 }
+  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "is an invalid address" }
+  validates :email, uniqueness: true
 end
