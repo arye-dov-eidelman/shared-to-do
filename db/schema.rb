c794e25a3bea6b40e37c15a4ae7136e42eab87a9
Arye Dov Eidelman <aryedov.e@gmail.com>
2019-02-07 11:46:13 -0500

    Add checked column to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_025305) do
+ActiveRecord::Schema.define(version: 2019_02_07_164104) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "checked", default: false
   end
 
   create_table "lists", force: :cascade do |t|
