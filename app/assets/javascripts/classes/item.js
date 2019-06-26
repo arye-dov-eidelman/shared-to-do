47883c89d6b0465c1d6b28c98732023a4d151b5e
Arye Eidelman <aryedov.e@gmail.com>
2019-06-26 17:37:37 -0400

    Create list item class

--- app/assets/javascripts/classes/item.js
+++ app/assets/javascripts/classes/item.js
@@ -1,3 +1,27 @@
 class Item {
+    constructor(data) {
+        return this.updateFromJson(data || {});
+    };
 
+    updateFromJson(data) {
+        if (!(data instanceof Object)) {
+            console.warn(`Item.updateFromJson() was called with '${data}' instead of json on:`, this, data)
+            return this;
+        }
+
+        if (data.id instanceof Number) {
+            this.id = data.id;
+        }
+
+        if (typeof(data.name) === 'string') {
+            this.name = data.name;
+        }
+
+        // converts any of the following to a boolean: "0", "1", 0, 1, true, false
+        this.checked = Boolean(Number(data.checked));
+
+        return this;
+    };
+
+    saved() { return (this.id instanceof Number); };
 }
